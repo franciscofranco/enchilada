074f3cb2b76333197892b846a14c097cb5d40e15
kimocoder <christian@aircrack-ng.org>
2019-04-19 09:53:38 +0200

    Cleanout

--- include/linux/hotcount.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HOTCOUNT_H_
-#define _HOTCOUNT_H_
-
-extern void insert_prio_node(unsigned int new_hotcount, uid_t uid);
-extern void delete_prio_node(uid_t uid);
-extern void print_prio_chain(struct seq_file *m);
-
-#endif
